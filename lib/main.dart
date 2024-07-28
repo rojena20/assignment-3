ec4dc8d22922e32882783fa6738e5faf6c2eacd6
Rojena <rojenamohamaden@gmail.com>
2024-07-28 20:28:33 +0300

    App without delete functionality

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:assignment3/contact_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: ContactScreen.routeName,
+    routes: {
+      ContactScreen.routeName: (context) => ContactScreen(),
+    },
+  ),);
+}
+
